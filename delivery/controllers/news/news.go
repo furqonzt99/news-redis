c3fe352871ee14ea74f6f012a5d4583dc714cf96
Muhammad Arif Furqon <furqonzt99@gmail.com>
2022-03-07 02:38:26 +0700

    news | implement redis services

--- delivery/controllers/news/news.go
+++ delivery/controllers/news/news.go
@@ -1,6 +1,7 @@
 package news
 
 import (
+	"encoding/json"
 	"net/http"
 	"strconv"
 	"strings"
@@ -8,8 +9,11 @@ import (
 	"github.com/furqonzt99/news-redis/delivery/common"
 	"github.com/furqonzt99/news-redis/domain/entity"
 	"github.com/furqonzt99/news-redis/domain/repository"
+	"github.com/furqonzt99/news-redis/services"
 	"github.com/labstack/echo/v4"
 )
+
+var newsEntity string = "news"
 
 type NewsController struct {
 	Repository repository.NewsInterface
@@ -38,6 +42,8 @@ func (nc NewsController) Create(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, common.NewBadRequestResponse())
 	}
 
+	go services.DeleteCache(newsEntity)
+
 	return c.JSON(http.StatusOK, common.NewSuccessOperationResponse())
 }
 
@@ -47,12 +53,21 @@ func (nc NewsController) ReadAll(c echo.Context) error {
 		Status: c.QueryParam("status"),
 		Tags:   strings.Split(c.QueryParam("topic"), ","),
 	}
+
+	response := []newsResponse{}
+
+	// get data from cache
+	newsCache, err := services.GetCache(newsEntity, 0, newsFilter)
+	if err == nil {
+		// Unmarshal response
+		_ = json.Unmarshal([]byte(newsCache), &response)
+		return c.JSON(http.StatusOK, common.SuccessResponseWithData(response))
+	}
+
 	newsDB, err := nc.Repository.ReadAll(newsFilter)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, common.NewBadRequestResponse())
 	}
-
-	response := []newsResponse{}
 
 	for _, news := range newsDB {
 		if len(news.Tags) > 0 {
@@ -73,6 +88,12 @@ func (nc NewsController) ReadAll(c echo.Context) error {
 		}
 	}
 
+	// Marshal response
+	resMarshal, _ := json.Marshal(response)
+
+	// Create cache
+	go services.CreateCache(newsEntity, 0, newsFilter, resMarshal)
+
 	return c.JSON(http.StatusOK, common.SuccessResponseWithData(response))
 }
 
@@ -82,6 +103,17 @@ func (nc NewsController) ReadOne(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, common.NewBadRequestResponse())
 	}
+
+	response := newsResponse{}
+
+	// get data from cache
+	newsCache, err := services.GetCache(newsEntity, newsID, "")
+	if err == nil {
+		// Unmarshal response
+		_ = json.Unmarshal([]byte(newsCache), &response)
+		return c.JSON(http.StatusOK, common.SuccessResponseWithData(response))
+	}
+
 	newsDB, err := nc.Repository.ReadOne(newsID)
 	if err != nil {
 		return c.JSON(http.StatusNotFound, common.NewNotFoundResponse())
@@ -93,13 +125,19 @@ func (nc NewsController) ReadOne(c echo.Context) error {
 		tags = append(tags, tag.Name)
 	}
 
-	response := newsResponse{
+	response = newsResponse{
 		ID:     newsID,
 		Title:  newsDB.Title,
 		Body:   newsDB.Body,
 		Status: newsDB.Status,
 		Tags:   tags,
 	}
+
+	// Marshal response
+	resMarshal, _ := json.Marshal(response)
+
+	// Create cache
+	go services.CreateCache(newsEntity, newsID, "", resMarshal)
 
 	return c.JSON(http.StatusOK, common.SuccessResponseWithData(response))
 }
@@ -128,6 +166,8 @@ func (nc NewsController) Edit(c echo.Context) error {
 		return c.JSON(http.StatusNotFound, common.NewNotFoundResponse())
 	}
 
+	go services.DeleteCache(newsEntity)
+
 	return c.JSON(http.StatusOK, common.NewSuccessOperationResponse())
 }
 
@@ -141,6 +181,8 @@ func (nc NewsController) Delete(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusNotFound, common.NewNotFoundResponse())
 	}
+
+	go services.DeleteCache(newsEntity)
 
 	return c.JSON(http.StatusOK, common.NewSuccessOperationResponse())
 }
@@ -156,6 +198,8 @@ func (nc NewsController) SetStatusDeleted(c echo.Context) error {
 		return c.JSON(http.StatusNotFound, common.NewNotFoundResponse())
 	}
 
+	go services.DeleteCache(newsEntity)
+
 	return c.JSON(http.StatusOK, common.NewSuccessOperationResponse())
 }
 
@@ -170,6 +214,8 @@ func (nc NewsController) SetStatusPublish(c echo.Context) error {
 		return c.JSON(http.StatusNotFound, common.NewNotFoundResponse())
 	}
 
+	go services.DeleteCache(newsEntity)
+
 	return c.JSON(http.StatusOK, common.NewSuccessOperationResponse())
 }
 
@@ -183,6 +229,8 @@ func (nc NewsController) SetStatusDraft(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusNotFound, common.NewNotFoundResponse())
 	}
+
+	go services.DeleteCache(newsEntity)
 
 	return c.JSON(http.StatusOK, common.NewSuccessOperationResponse())
 }
